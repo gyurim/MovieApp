e5e5415efa90635c1bcab86b6b5dd9a678767aef
gyurim <gyurim1250@gmail.com>
2022-11-15 21:32:01 +0900

    feat - movie detail 화면 출력

---
+++ app/src/main/java/com/gyurim/movieapp/presentation/ui/detail/MovieDetailActivity.kt
@@ -0,0 +1,68 @@
+package com.gyurim.movieapp.presentation.ui.detail
+
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.gyurim.movieapp.R
+import com.gyurim.movieapp.data.remote.model.Movie
+import com.gyurim.movieapp.databinding.ActivityMovieDetailBinding
+
+class MovieDetailActivity: AppCompatActivity() {
+    private var movie : Movie = Movie("", "", "", "", "", 0.0)
+    private lateinit var binding: ActivityMovieDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail)
+        binding.lifecycleOwner = this
+
+        intent?.getParcelableExtra<Movie>(MOVIE_DETAIL_DATA)?.run {
+            movie = this
+            binding.movie = movie
+        }
+
+        initToolbar()
+        loadWebView()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadWebView() {
+        binding.movieWebView.settings.apply {
+            setSupportMultipleWindows(false)
+            setSupportZoom(true)
+            builtInZoomControls = true
+            javaScriptEnabled = true
+            displayZoomControls = false
+        }
+
+        binding.movieWebView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(movie.link)
+            Log.d("link", movie.link)
+        }
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.movieDetailToolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            title = movie.title
+        }
+    }
+
+    companion object {
+        const val MOVIE_DETAIL_DATA = "MOVIE_DETAIL"
+    }
+
+}
